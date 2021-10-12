bde709324311d5fcc4f02b4acc7a00de5f50da5c
Saha <50802940+Sasha5673@users.noreply.github.com>
2021-10-12 20:07:34 +0300

    Add files via upload

---
+++ Homework2.php
@@ -0,0 +1,16 @@
+<?php
+$name = 'Alexandr';
+echo $name . "\n";
+$age = 18;
+echo $age . "\n";
+$pi = 3.14;
+echo $pi . "\n";
+$arr1 = ['alex', 'vova', 'tolya'];
+print_r($arr1);
+$arr2 = ['alex', 'vova', 'tolya', ['kostya', 'olya']];
+print_r($arr2);
+$arr3 = ['alex', 'vova', 'tolya', ['kostya', 'olya', ['gosha', 'mila']]];
+print_r($arr3);
+$arr4 = [['Alex', 'Vova', 'Tolya'], ['Kostya', 'Olya'], ['Gosha', 'Mila']];
+print_r($arr4);
+?>
